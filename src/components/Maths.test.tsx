008f21d30d58022f96ad0537e4b3549c9874135e
Hayley Ashby <hayleyashby7@gmail.com>
2023-07-20 09:51:51 +0100

    test: :white_check_mark: Maths component tests completed

--- src/components/Maths.test.tsx
+++ src/components/Maths.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Maths from './Maths';
 import { ChangeEvent } from 'react';
+import userEvent from '@testing-library/user-event';
 
 test('renders Maths component', () => {
 	// Arrange
@@ -41,28 +42,44 @@ test('renders Maths component with valid props', () => {
 	// Assert
 	expect(label).toBeInTheDocument();
 	expect(select).toBeInTheDocument();
-    expect(select).toHaveValue('4');
+	expect(select).toHaveValue('4');
 });
 
 test('renders Maths component appropriately with invalid props', () => {
-    // Arrange
-    render(
-        <Maths
-            maths='8'
-            onChangeUpdate={function (event: ChangeEvent<HTMLSelectElement>): void {
-                throw new Error('Function not implemented.');
-            }}
-        />
-    );
+	// Arrange
+	render(
+		<Maths
+			maths='8'
+			onChangeUpdate={function (event: ChangeEvent<HTMLSelectElement>): void {
+				throw new Error('Function not implemented.');
+			}}
+		/>
+	);
 
-    // Act
-    const label = screen.getByText(/What is 2 \+ 2\?/i);
-    const select = screen.getByRole('combobox');
+	// Act
+	const label = screen.getByText(/What is 2 \+ 2\?/i);
+	const select = screen.getByRole('combobox');
 
-    // Assert
-    expect(label).toBeInTheDocument();
-    expect(select).toBeInTheDocument();
-    expect(select).toHaveValue('Not 4');
+	// Assert
+	expect(label).toBeInTheDocument();
+	expect(select).toBeInTheDocument();
+	expect(select).toHaveValue('Not 4');
 });
 
+test('renders Maths component with props and updates', async () => {
+	// Arrange
+	let option = '';
 
+	const mockOnChangeUpdate = jest.fn((event: ChangeEvent<HTMLSelectElement>) => {
+		option += event.target.value;
+	});
+
+	render(<Maths maths='' onChangeUpdate={mockOnChangeUpdate} />);
+
+	// Act
+	await userEvent.selectOptions(screen.getByRole('combobox'), '4');
+
+	// Assert
+    expect(mockOnChangeUpdate).toHaveBeenCalledTimes(1);
+    expect(option).toBe('4');
+});

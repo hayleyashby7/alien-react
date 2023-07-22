122dce09680a0104adc039cb5bbfc972f811e942
Hayley Ashby <hayleyashby7@gmail.com>
2023-07-22 12:47:08 +0100

    Input validation for planet component including tests

--- src/components/Planet.test.tsx
+++ src/components/Planet.test.tsx
@@ -62,3 +62,63 @@ test('renders Planet component with props and updates', async () => {
 	expect(mockOnChangeUpdate).toHaveBeenCalledTimes(5);
 	expect(text).toBe('Earth');
 });
+
+test('renders Planet component with props and updates with error if too few characters', async () => {
+	// Arrange
+	let text = '';
+
+	const mockOnChangeUpdate = jest.fn((event: ChangeEvent<HTMLInputElement>) => {
+		text += event.target.value;
+	});
+
+	render(<Planet planetName={''} onChangeUpdate={mockOnChangeUpdate} />);
+
+	// Act
+	await userEvent.type(screen.getByRole('textbox'), 'E');
+
+	// Assert
+	expect(mockOnChangeUpdate).toHaveBeenCalledTimes(1);
+	expect(text).toBe('E');
+	expect(screen.getByRole('alert')).toBeInTheDocument();
+	expect(screen.getByRole('alert')).toHaveTextContent('Planet Name must be at least 2 characters');
+});
+
+test('renders Planet component with props and updates with error if too many characters', async () => {
+	// Arrange
+	let text = '';
+
+	const mockOnChangeUpdate = jest.fn((event: ChangeEvent<HTMLInputElement>) => {
+		text += event.target.value;
+	});
+
+	render(<Planet planetName={'abcdefghijklmnopqrstuvwxyz1234567891011121314151617'} onChangeUpdate={mockOnChangeUpdate} />);
+
+	// Act
+	await userEvent.type(screen.getByRole('textbox'), 'E');
+
+	// Assert
+	expect(mockOnChangeUpdate).toHaveBeenCalledTimes(1);
+	expect(text).toBe('abcdefghijklmnopqrstuvwxyz1234567891011121314151617E');
+	expect(screen.getByRole('alert')).toBeInTheDocument();
+	expect(screen.getByRole('alert')).toHaveTextContent('Planet Name must be less than 49 characters');
+});
+
+test('renders Planet component with props and updates with error if special characters', async () => {
+	// Arrange
+	let text = '';
+
+	const mockOnChangeUpdate = jest.fn((event: ChangeEvent<HTMLInputElement>) => {
+		text += event.target.value;
+	});
+
+	render(<Planet planetName={'Earth'} onChangeUpdate={mockOnChangeUpdate} />);
+
+	// Act
+	await userEvent.type(screen.getByRole('textbox'), '!');
+
+	// Assert
+	expect(mockOnChangeUpdate).toHaveBeenCalledTimes(1);
+	expect(text).toBe('Earth!');
+	expect(screen.getByRole('alert')).toBeInTheDocument();
+	expect(screen.getByRole('alert')).toHaveTextContent('No special characters allowed');
+});
--- src/components/Planet.tsx
+++ src/components/Planet.tsx
@@ -1,14 +1,40 @@
+import { useState } from 'react';
+import ErrorMessage from './ErrorMessage';
+
 interface PlanetProps {
 	planetName: string;
 	onChangeUpdate: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const Planet: React.FC<PlanetProps> = ({ planetName, onChangeUpdate }) => {
+	const [errorMessage, setErrorMessage] = useState<string | undefined>();
+
+	const validatePlanetName = (value: string): string | undefined => {
+		return value.length === undefined
+			? 'Planet Name is required'
+			: value.length < 2
+			? 'Planet Name must be at least 2 characters'
+			: value.length > 49
+			? 'Planet Name must be less than 49 characters'
+			: value.match(/^[a-zA-Z0-9]+$/) === null
+			? 'No special characters allowed'
+			: undefined;
+	};
+
+	const onChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+		const error = validatePlanetName(event.target.value);
+		setErrorMessage(error);
+		onChangeUpdate(event);
+	};
+
 	return (
-		<div className='formInput'>
-			<label htmlFor='planetName'>Planet Name:</label>
-			<input type='text' id='planetName' value={planetName} onChange={onChangeUpdate} />
-		</div>
+		<>
+			<div className='formInput'>
+				<label htmlFor='planetName'>Planet Name:</label>
+				<input type='text' id='planetName' value={planetName} onChange={onChange} />
+			</div>
+			<ErrorMessage message={errorMessage} />
+		</>
 	);
 };
 

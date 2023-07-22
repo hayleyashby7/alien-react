75fb10435530c25c02fd88796007b0b149034a15
Hayley Ashby <hayleyashby7@gmail.com>
2023-07-22 13:22:54 +0100

    feat: :sparkles: Input validation for maths component including tests

--- src/components/Maths.test.tsx
+++ src/components/Maths.test.tsx
@@ -66,7 +66,7 @@ test('renders Maths component appropriately with invalid props', () => {
 	expect(select).toHaveValue('Not 4');
 });
 
-test('renders Maths component with props and updates', async () => {
+test('renders Maths component with props and updates without error if 4 selected', async () => {
 	// Arrange
 	let option = '';
 
@@ -80,6 +80,48 @@ test('renders Maths component with props and updates', async () => {
 	await userEvent.selectOptions(screen.getByRole('combobox'), '4');
 
 	// Assert
-    expect(mockOnChangeUpdate).toHaveBeenCalledTimes(1);
-    expect(option).toBe('4');
+	expect(mockOnChangeUpdate).toHaveBeenCalledTimes(1);
+	expect(option).toBe('4');
+	expect(screen.getByRole('alert')).toBeInTheDocument();
+	expect(screen.getByRole('alert')).toHaveTextContent('');
+});
+
+test('renders Maths component with props and updates with error if 4 is not selected', async () => {
+	// Arrange
+	let option = '';
+
+	const mockOnChangeUpdate = jest.fn((event: ChangeEvent<HTMLSelectElement>) => {
+		option += event.target.value;
+	});
+
+	render(<Maths maths='' onChangeUpdate={mockOnChangeUpdate} />);
+
+	// Act
+	await userEvent.selectOptions(screen.getByRole('combobox'), 'Not 4');
+
+	// Assert
+	expect(mockOnChangeUpdate).toHaveBeenCalledTimes(1);
+	expect(option).toBe('Not 4');
+	expect(screen.getByRole('alert')).toBeInTheDocument();
+	expect(screen.getByRole('alert')).toHaveTextContent('2 + 2 is 4');
+});
+
+test('renders Maths component with props and updates with error if no option is selected', async () => {
+	// Arrange
+	let option = '';
+
+	const mockOnChangeUpdate = jest.fn((event: ChangeEvent<HTMLSelectElement>) => {
+		option += event.target.value;
+	});
+
+	render(<Maths maths='' onChangeUpdate={mockOnChangeUpdate} />);
+
+	// Act
+	await userEvent.selectOptions(screen.getByRole('combobox'), '');
+
+	// Assert
+	expect(mockOnChangeUpdate).toHaveBeenCalledTimes(1);
+	expect(option).toBe('');
+	expect(screen.getByRole('alert')).toBeInTheDocument();
+	expect(screen.getByRole('alert')).toHaveTextContent('Answer is required');
 });
--- src/components/Maths.tsx
+++ src/components/Maths.tsx
@@ -1,21 +1,39 @@
+import { useState } from 'react';
+import ErrorMessage from './ErrorMessage';
+
 interface MathsProps {
 	maths: string;
 	onChangeUpdate: (event: React.ChangeEvent<HTMLSelectElement>) => void;
 }
 
 const Maths: React.FC<MathsProps> = ({ maths, onChangeUpdate }) => {
+	const [errorMessage, setErrorMessage] = useState<string | undefined>();
+
 	// Validate maths prop
 	if (maths !== '4' && maths !== '') maths = 'Not 4';
 
+	const validateMathsInput = (value: string): string | undefined => {
+		return value.length === undefined || value === '' ? 'Answer is required' : value !== '4' ? '2 + 2 is 4' : undefined;
+	};
+
+	const onChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+		const error = validateMathsInput(event.target.value);
+		setErrorMessage(error);
+		onChangeUpdate(event);
+	};
+
 	return (
-		<div className='formInput'>
-			<label htmlFor='maths'>What is 2 + 2?</label>
-			<select id='maths' value={maths} onChange={onChangeUpdate}>
-				<option value=''>Choose answer</option>
-				<option value='4'>4</option>
-				<option value='Not 4'>Not 4</option>
-			</select>
-		</div>
+		<>
+			<div className='formInput'>
+				<label htmlFor='maths'>What is 2 + 2?</label>
+				<select id='maths' value={maths} onChange={onChange}>
+					<option value=''>Choose answer</option>
+					<option value='4'>4</option>
+					<option value='Not 4'>Not 4</option>
+				</select>
+			</div>
+			<ErrorMessage message={errorMessage} />
+		</>
 	);
 };
 

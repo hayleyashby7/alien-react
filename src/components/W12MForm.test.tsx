1dd2ba60159b37da683c60c2c9d038d20773fe78
Hayley Ashby <hayleyashby7@gmail.com>
2023-07-22 11:02:55 +0100

    test: :white_check_mark: Updated W12MForm component tests

--- src/components/W12MForm.test.tsx
+++ src/components/W12MForm.test.tsx
@@ -1,12 +1,58 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import W12MForm from './W12MForm';
+import userEvent from '@testing-library/user-event';
 
-test('renders form element', () => {
-	// we can hold onto the object returned from render()
-	// this object has a container property that we can destructure and inspect
-	const { container } = render(<W12MForm />);
+test('renders W12MForm component', () => {
+	// Arrange
+	render(<W12MForm />);
 
-	// the container is just a normal DOM element, so we can look at normal properties like '.firstChild'
-	// for example, the firstChild of our container should be our form element
-	expect(container.firstChild).toHaveClass('w12MForm');
+	// Act
+	const header = screen.getByText(/Each species may only submit ONE W-12-M form./i);
+	const speciesLabel = screen.getByText(/Species Name:/i);
+	const speciesInput = screen.getByRole('textbox', { name: /species name/i });
+	const planetLabel = screen.getByText(/Planet Name:/i);
+	const planetInput = screen.getByRole('textbox', { name: /planet/i });
+	const beingsLabel = screen.getByText(/Number of beings:/i);
+	const beingsInput = screen.getByRole('spinbutton', { name: /number of beings/i });
+	const mathsLabel = screen.getByText(/What is 2 \+ 2\?/i);
+	const mathsInput = screen.getByRole('combobox');
+	const reasonLabel = screen.getByText(/Reason for sparing:/i);
+	const reasonInput = screen.getByRole('textbox', { name: /reason for sparing/i });
+	const button = screen.getByRole('button', { name: /submit/i });
+
+	// Assert
+	expect(header).toBeInTheDocument();
+	expect(speciesLabel).toBeInTheDocument();
+	expect(speciesInput).toBeInTheDocument();
+	expect(planetLabel).toBeInTheDocument();
+	expect(planetInput).toBeInTheDocument();
+	expect(beingsLabel).toBeInTheDocument();
+	expect(beingsInput).toBeInTheDocument();
+	expect(mathsLabel).toBeInTheDocument();
+	expect(mathsInput).toBeInTheDocument();
+	expect(reasonLabel).toBeInTheDocument();
+	expect(reasonInput).toBeInTheDocument();
+	expect(button).toBeInTheDocument();
+});
+
+test('correct data submitted by W12MForm', async () => {
+	// Arrange
+	render(<W12MForm />);
+	const consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+
+	// Act
+	await userEvent.type(screen.getByRole('textbox', { name: /species name/i }), 'Human');
+	await userEvent.type(screen.getByRole('textbox', { name: /planet/i }), 'Earth');
+	await userEvent.type(screen.getByRole('spinbutton', { name: /number of beings/i }), '1');
+	await userEvent.selectOptions(screen.getByRole('combobox'), '4');
+	await userEvent.type(screen.getByRole('textbox', { name: /reason for sparing/i }), 'I am human');
+	await userEvent.click(screen.getByRole('button', { name: /submit/i }));
+
+	// Assert
+	expect(consoleSpy).toHaveBeenCalledTimes(5);
+	expect(consoleSpy).toHaveBeenCalledWith('Species: ', 'Human');
+	expect(consoleSpy).toHaveBeenCalledWith('Planet: ', 'Earth');
+	expect(consoleSpy).toHaveBeenCalledWith('Beings: ', 1);
+	expect(consoleSpy).toHaveBeenCalledWith('Maths: ', '4');
+	expect(consoleSpy).toHaveBeenCalledWith('Reason: ', 'I am human');
 });
